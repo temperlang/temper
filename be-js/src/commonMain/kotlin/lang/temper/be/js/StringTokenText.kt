fa6e2c568344fb5af93509d99c5e8dbc863d841c
Mike Samuel <mikesamuel@gmail.com>
2026-02-24 14:29:02 -0700

    simplified JS string rendering by removing unnecessary backtick form
    Signed-off-by: Mike Samuel <mikesamuel@gmail.com>

--- be-js/src/commonMain/kotlin/lang/temper/be/js/StringTokenText.kt
+++ be-js/src/commonMain/kotlin/lang/temper/be/js/StringTokenText.kt
@@ -1,54 +1,24 @@
 package lang.temper.be.js
 
-import lang.temper.common.C_BQ
 import lang.temper.common.C_BS
 import lang.temper.common.C_CR
-import lang.temper.common.C_DOL
 import lang.temper.common.C_DQ
 import lang.temper.common.C_LF
 import lang.temper.common.C_SLASH
 import lang.temper.common.C_SQ
 import lang.temper.common.C_TAB
 
-private const val CRLF_MASK = (1L shl C_CR) or (1L shl C_LF)
-
 @Suppress("MagicNumber") // Lots of bit twiddling
 internal fun stringTokenText(value: String): String {
-    val n = value.length
     var lowUsedMask = 0L
-    var highUsedMask = 0L
     for (c in value) {
         when (val code = c.code) {
             in 0..63 -> lowUsedMask = lowUsedMask or (1L shl code)
-            in 64..127 -> highUsedMask = highUsedMask or (1L shl (code - 64))
         }
     }
 
     val hasSingleQuote = (lowUsedMask and (1L shl C_SQ)) != 0L
     val hasDoubleQuote = (lowUsedMask and (1L shl C_DQ)) != 0L
-    val hasBackSlash = (lowUsedMask and (1L shl C_BS)) != 0L
-    val controls = (lowUsedMask and (0xFFFF_FFFFL))
-    val hasBacktick = (highUsedMask and (1L shl (C_BQ - 64))) != 0L
-    val hasDollar = (lowUsedMask and (1L shl C_DOL)) != 0L
-    val hasTemplateStringSpecial = hasBacktick or hasDollar or hasBackSlash
-
-    if (!hasTemplateStringSpecial && (controls and CRLF_MASK.inv()) == 0L) {
-        var lineBreakCount = 0
-        var i = 0
-        while (i < n) {
-            val c = value[i]
-            i += 1
-            if (c.isJsLineTerminatorChar) {
-                lineBreakCount++
-                if (i < n && c == '\r' && value[i] == '\n') {
-                    i += 1 // Count CR LF as one terminator
-                }
-            }
-        }
-        if (lineBreakCount >= BACKQUOTED_LF_COUNT_THRESHOLD) {
-            return "`$value`"
-        }
-    }
 
     val delimiter = when {
         hasSingleQuote == hasDoubleQuote -> '"'
@@ -82,8 +52,6 @@ internal fun stringTokenText(value: String): String {
         append(delimiter)
     }
 }
-
-internal const val BACKQUOTED_LF_COUNT_THRESHOLD = 5
 
 internal val Char.isJsLineTerminatorChar get() = when (this) {
     '\n', '\r', '\u2028', '\u2029' -> true
