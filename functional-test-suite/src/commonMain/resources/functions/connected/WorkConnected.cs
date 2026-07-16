85df61285820393862005be8198ff0915ef6c3c6
Tom <tom@temper.systems>
2026-07-16 08:15:59 -0700

    Pass csharp connected funtest
    Signed-off-by: Tom <tom@temper.systems>

---
+++ functional-test-suite/src/commonMain/resources/functions/connected/WorkConnected.cs
@@ -0,0 +1,15 @@
+namespace Work
+{
+    static class WorkConnected
+    {
+        internal static int Sum(int i, int j, int bonus)
+        {
+            return i + j + bonus;
+        }
+
+        internal static int Prod(Hidden hidden, int j)
+        {
+            return new MoreSupport().Prod(hidden.I, j);
+        }
+    }
+}
