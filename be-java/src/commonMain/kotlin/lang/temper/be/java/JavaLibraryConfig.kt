016a22a87df00d2ac3aa75f8cd53c6ddb31d67b1
Tom <tom@temper.systems>
2026-09-09 18:30:11 -0700

    Move java config back to be-java
    Signed-off-by: Tom <tom@temper.systems>

--- be-java/src/commonMain/kotlin/lang/temper/be/java/JavaLibraryConfig.kt
+++ be-java/src/commonMain/kotlin/lang/temper/be/java/JavaLibraryConfig.kt
@@ -1,22 +1,29 @@
 package lang.temper.be.java
 
+import lang.temper.builtin.BuiltinFuns
+import lang.temper.builtin.Types
 import lang.temper.common.console
 import lang.temper.common.subListToEnd
-import lang.temper.frontend.staging.backend.JavaConfigKeys
+import lang.temper.frontend.BindingsInjector
+import lang.temper.frontend.Module
+import lang.temper.frontend.staging.buildConfigType
 import lang.temper.library.LibraryConfiguration
 import lang.temper.library.LibraryConfigurations
 import lang.temper.library.backendLibraryName
 import lang.temper.library.versionOrDefault
 import lang.temper.log.FilePath
 import lang.temper.log.FilePathSegment
+import lang.temper.log.LogSink
 import lang.temper.name.ExportedName
 import lang.temper.name.ModuleName
 import lang.temper.name.SourceName
 import lang.temper.name.Symbol
+import lang.temper.value.BlockTree
 import lang.temper.value.InstancePropertyRecord
 import lang.temper.value.TClass
 import lang.temper.value.TList
 import lang.temper.value.TString
+import lang.temper.value.Value
 
 open class JavaLibraryConfigs(
     val base: LibraryConfigurations,
@@ -136,4 +143,54 @@ class JavaLibraryConfig(
                 null -> listOf(libraryName)
                 else -> javaPackageMetadataString.split(".")
             }
+}
+
+object JavaConfigKeys {
+    /** Key for the be-java (and be-java8) config instance. */
+    const val CONFIG = "java"
+
+    /** The name of the class for configuring be-java. */
+    const val CONFIG_CLASS_NAME = "JavaConfig"
+
+    /** Config files may export a name with this text to specify the Maven library name */
+    const val NAME = "name"
+    val libraryNameGlobal = Symbol("javaName")
+
+    /** Config files may export a name with this text to specify the Maven group id */
+    const val GROUP = "group"
+    val libraryGroupGlobal = Symbol("javaGroup")
+
+    /** Config files may export a name with this text to specify the Maven artifact id */
+    const val ARTIFACT = "artifact"
+    val libraryArtifactGlobal = Symbol("javaArtifact")
+
+    /** Config files may export a name with this text to specify the Java `package` name */
+    const val PACKAGE = "package"
+    val packageGlobal = Symbol("javaPackage")
+
+    /** Config key to specify Maven dependencies */
+    const val DEPENDENCIES = "dependencies"
+    val dependenciesGlobal = Symbol("javaDependencies")
+}
+
+object JavaConfigInjector : BindingsInjector {
+    override fun inject(module: Module, root: BlockTree, logSink: LogSink) {
+        root.insert {
+            buildConfigType(
+                name = JavaConfigKeys.CONFIG_CLASS_NAME,
+                properties = mapOf(
+                    JavaConfigKeys.NAME to { V(Value(Types.string)) },
+                    JavaConfigKeys.PACKAGE to { V(Value(Types.string)) },
+                    JavaConfigKeys.GROUP to { V(Value(Types.string)) },
+                    JavaConfigKeys.ARTIFACT to { V(Value(Types.string)) },
+                    JavaConfigKeys.DEPENDENCIES to {
+                        Call(BuiltinFuns.angleFn) {
+                            V(Value(Types.list))
+                            V(Value(Types.string))
+                        }
+                    },
+                ),
+            )
+        }
+    }
 }
