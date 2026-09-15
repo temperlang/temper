918d9155979d088001ceb7b4215b8a4de216737e
Mike Samuel <mikesamuel@gmail.com>
2026-09-15 14:27:26 -0600

    cleanup and documented
    Signed-off-by: Mike Samuel <mikesamuel@gmail.com>

--- frontend/src/commonTest/kotlin/lang/temper/frontend/InterpreterFunctionalTests.kt
+++ frontend/src/commonTest/kotlin/lang/temper/frontend/InterpreterFunctionalTests.kt
@@ -68,11 +68,6 @@ class InterpreterFunctionalTests : FunctionalTestSuiteI {
         super.algosHelloWorld()
     }
 
-    @Test
-    override fun typesMap() {
-        super.typesMap()
-    }
-
     override fun runFunctionalTest(
         test: FunctionalTestBase,
         verbose: Boolean,
